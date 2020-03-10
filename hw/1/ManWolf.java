8b753e4e12584c6770fef7c36bc3d65f27fdb1a6
Andrew DiBella <andrew.dibella1@marist.edu>
2020-03-10 13:04:58 -0400

    dfa implementaion working

--- hw/1/ManWolf.java
+++ hw/1/ManWolf.java
@@ -1,4 +1,4 @@
-abstract class ManWolf{  
+class ManWolf{  
 
   private static final int q0  = 0;
   private static final int q1  = 1;
@@ -10,9 +10,38 @@ abstract class ManWolf{
   private static final int q7  = 7;
   private static final int q8  = 8;
   private static final int q9  = 9;
-  private static final int q10 = 10;
+  private static final int qe = 10;
 
+  private static int state;
+  private static int[][] delta = {
+    //    | q0 | q1 | q2 | q3 | q4 | q5 | q6 | q7 | q8 | q9 | q10 |
+    /*g*/ { q1 , q0 , qe , q4 , q3 , q6 , q5 , qe , q9 , q8 , qe },
+    /*w*/ { qe , qe , q3 , q2 , qe , qe , q7 , q6 , qe , qe , qe },
+    /*c*/ { qe , qe , q5 , qe , q7 , q2 , qe , q4 , qe , qe , qe },
+    /*n*/ { qe , q2 , q1 , qe , qe , qe , qe , q8 , q7 , qe , qe },
+  };
   ManWolf(){
+    state = 0;
+  }
+  // checks wether the dfa reached the accepting state
+  boolean isAccepted(){
+    return state == q9;
+  }
 
+  void process(String str){
+    for (int i = 0; i < str.length(); i++){
+      char c = str.charAt(i);
+      if(c == 'g'){
+        state = delta[0][state];
+      }else if (c == 'w'){
+        state = delta[1][state];
+      }else if (c == 'c'){
+        state = delta[2][state];
+      }else if (c == 'n'){
+        state = delta[3][state];
+      }else{ 
+        state = qe;
+      }
+    }
   }
 }
--- hw/1/driverDFA.java
+++ hw/1/driverDFA.java
@@ -1,9 +1,32 @@
+/*
+ *  Author: Andrew DiBella
+ *  ManWolfCabbage  DFA 
+ *  Driver reads from plain text input 
+ *  
+ *  Command line: java driverDFA <string: g,w,c,n>
+ *  ManWolf.java requried 
+ *  
+ *  If input string is a valid sequence for the Man Wolf problem
+ *  System prints if is a correct solution. 
+ */
 
-import java.util.Scanner;
+import java.io.IOException;
 class driverDFA{
+  public static void main(String [] args) throws IOException{
+    try{
+      ManWolf dfa = new ManWolf();
+      String sequence = args[0];
+      sequence = sequence.toLowerCase();
 
-  public static void main(String [] args){
-    
+      // checks input for valid sequence
+      dfa.process(sequence);
+      if(dfa.isAccepted()){
+        System.out.println("This is a solution.");
+      } else{
+        System.out.println("This is not a solution.");
+      }
+    }catch(ArrayIndexOutOfBoundsException e){
+      System.out.println("Enter string with: g w c n characters."); 
+    }
   }
-
 }
