6b03264e0ca6e6a8791ac72ce82318ef47c60565
andrewdibs <Andrew.DiBella1@marist.edu>
2020-03-10 10:36:55 -0400

    added files

---
+++ hw/1/ManWolf.java
@@ -0,0 +1,18 @@
+abstract class ManWolf{  
+
+  private static final int q0  = 0;
+  private static final int q1  = 1;
+  private static final int q2  = 2;
+  private static final int q3  = 3;
+  private static final int q4  = 4;
+  private static final int q5  = 5;
+  private static final int q6  = 6;
+  private static final int q7  = 7;
+  private static final int q8  = 8;
+  private static final int q9  = 9;
+  private static final int q10 = 10;
+
+  ManWolf(){
+
+  }
+}
---
+++ hw/1/driverDFA.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+class driverDFA{
+
+  public static void main(String [] args){
+    
+  }
+
+}
